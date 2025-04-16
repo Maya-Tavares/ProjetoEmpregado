21c6e288c516f2ef35cc459ccf6e81ffe540e006
Maya Tavares <tavares.pereira@aluno.ifsp.edu.br>
2025-04-16 20:59:27 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         Empregado[] empregado = new Empregado[3];
-        empregado[0] = new EmpregadoComissionado();
-        empregado[1] = new EmpregadoHorista();
-        empregado[2] = new EmpregadoHorista();
+        empregado[0] = new EmpregadoComissionado(559111, "grazi", 120, 5 );
+        empregado[1] = new EmpregadoHorista(559031, "maya", 50, 100);
+        empregado[2] = new EmpregadoHorista(123, "invi", 40, 200);
 
         for (Empregado e : empregado) {
             System.out.println("Funcionário " + e.nome);
