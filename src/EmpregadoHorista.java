de5b22b4a4a745e3b925254d1f0e126308fc0a9a
Maya Tavares <tavares.pereira@aluno.ifsp.edu.br>
2025-04-16 20:59:25 -0300

    Update EmpregadoHorista.java

--- src/EmpregadoHorista.java
+++ src/EmpregadoHorista.java
@@ -3,9 +3,20 @@ public class EmpregadoHorista extends Empregado{
     int totalHorasTrabalhadas;
     double valorHora;
 
+    public EmpregadoHorista(long matricula, String nome, int totalHorasTrabalhadas, double valorHora){
+        super(matricula, nome);
+        this.totalHorasTrabalhadas = totalHorasTrabalhadas;
+        this.valorHora = valorHora;
+    }
+
     public double calcularSalario(int totalHorasTrabalhadas, double valorHora) {
 //        return super.calcularSalario();
         double salario = totalHorasTrabalhadas * valorHora;
         return salario;
+    }
+
+    @Override
+    public String getDados() {
+        return super.getDados() + " - valor hora: " + valorHora + " - total: R$ " + totalHorasTrabalhadas;
     }
 }
